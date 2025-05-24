6285a93c1237c08dec51e772d009f98c7db19d4c
MiniOrange1 <lucas.cunha.souza@escola.pr.gov.br>
2025-05-24 13:35:21 -0300

    Add files via upload

---
+++ ProjetoWebAula-6/perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'Pergunta 1',
+    'O que são as esferas do dragão?',
+    'São objetos que concedem desejos quando juntas'
+)
+
+criaCartao(
+    'Pergunta 2',
+    'Quem é o mais forte da saga buu?',
+    'Vegito em sua forma SSJ'
+)
+
+criaCartao(
+    'Pergunta 3',
+    'Qual o maior jogo de Dragon ball na atualidade?',
+    'Dragon Ball Legends'
+)
