c0993ffd778da2e39920e67b0e472e695d4a2232
MiniOrange1 <lucas.cunha.souza@escola.pr.gov.br>
2025-05-24 14:15:28 -0300

    Add files via upload

---
+++ ProjetoWebAula-9/perguntas.js
@@ -0,0 +1,22 @@
+criaCartao(
+    'Pergunta 1',
+    'O que são as esferas do dragão?',
+    'São objetos que concedem desejos quando juntas'
+)
+
+criaCartao(
+    'Pergunta 2',
+    'Quem é o mais forte da saga buu?',
+    'Vegito em sua forma SSJ'
+)
+
+criaCartao(
+    'Pergunta 3',
+    'Qual o maior jogo de Dragon ball na atualidade?',
+    'Dragon Ball Legends'
+)
+criaCartao(
+    'Pergunta 4',
+    'Qual o segundo protagonista de dragon ball?',
+    'Gohan, Filho de Son Goku'
+)
