44fdc9284c9054b8c8971da7a7caa9249b185aa6
MiniOrange1 <lucas.cunha.souza@escola.pr.gov.br>
2025-05-27 16:04:21 -0300

    Add files via upload

--- ProjetoWebAula-10/perguntas.js
+++ ProjetoWebAula-10/perguntas.js
@@ -19,4 +19,31 @@ criaCartao(
     'Pergunta 4',
     'Qual o segundo protagonista de dragon ball?',
     'Gohan, Filho de Son Goku'
+) 
+criaCartao(
+    'Pergunta 5',
+    'Qual o primeiro personagem que goku encontra no ínicio de sua aventura?',
+    'Bulma'
+)
+
+criaCartao(
+    'Pergunta 6',
+    'Qual foi a transformação de Goku que quebrou a internet?',
+    'O Instinto Superior'
+)
+
+criaCartao(
+    'Pergunta 7',
+    'Qual foi a primeira vez que Goku lutou contra um alienígena?',
+    'Quando enfrentou o rei demónio piccolo'
+)
+criaCartao(
+    'Pergunta 8',
+    'Quanto durou a luta de Goku e frieza?',
+    '4 horas e 13 minutos'
+)
+criaCartao(
+    'Pergunta 9',
+    'Qual é o dia internacional do Goku?',
+    '9 de maio'
 )
